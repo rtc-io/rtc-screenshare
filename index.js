d51f8fa50267643815935f72609974ec64872876
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-08 08:06:37 +1000

    gendocs - include extension link

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ var screenshare = (detect.moz ? require('./moz') : require('./chrome'));
   [`chrome.desktopCapture`](https://developer.chrome.com/extensions/desktopCapture)
   API, and communicates with this module through `postMessage` calls.
 
+  An early, installable version of the extension is available in the
+  [Chrome Web Store](https://chrome.google.com/webstore/detail/webrtc-screen-sharing-for/einjngigaajacmojcohefgmnhhdnllic).
+
+  __NOTE:__ The extension is not publicly available yet, but using the direct link
+  you can install it.
+
+  ## Give it a Try
+
+  Once you have the plugin installed, you should be able to take the screenshare
+  module for a run using [requirebin](http://requirebin.com/?gist=6dcd5ced3964f0b3c40a)
+
+  This is, however, an early version of both the package and chrome extension so
+  we will be working through any
+  [issues](https://github.com/rtc-io/rtc-screenshare/issues) before removing
+  the __experimental__ status.
+
 **/
 
 ['window'].forEach(function(target) {
