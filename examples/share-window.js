f501a9d253765e668376868a7e716fc470634b56
Damon Oehlman <damon.oehlman@gmail.com>
2014-09-26 09:34:37 +1000

    Add error handling and logging to the example

--- examples/share-window.js
+++ examples/share-window.js
@@ -2,5 +2,17 @@ var screenshare = require('..');
 var media = require('rtc-media');
 
 screenshare.window(function(err, constraints) {
-  media(constraints).render(document.body);
+  if (err) {
+    return console.error('Could not capture window: ', err);
+  }
+
+  console.log('attempting capture with constraints: ', constraints);
+  media({ constraints: constraints })
+    .on('error', function(err) {
+      console.error('Could not capture: ', err);
+    })
+    .once('capture', function(stream) {
+      console.log('Successfully captured stream: ', stream);
+    })
+    .render(document.body);
 });
