a4c15c3b26dc54bc81dee12034916544902f5f8b
Damon Oehlman <damon.oehlman@gmail.com>
2014-12-19 12:01:27 +1100

    Progress on better chrome app integration

--- chrome.js
+++ chrome.js
@@ -1,5 +1,8 @@
+var defaults = require('cog/defaults');
+var crel = require('crel');
+
 module.exports = function(opts, callback) {
-  var targets;
+  var doc = typeof document != 'undefined' && document;
 
   // remap args
   if (typeof opts == 'function') {
@@ -7,8 +10,20 @@ module.exports = function(opts, callback) {
     opts = {};
   }
 
-  // get the target
-  targets = (opts || {}).targets || ['window', 'screen'];
+  // initialise defaults
+  opts = defaults({}, opts, {
+    targets: ['window', 'screen'],
+    inlineInstall: true,
+
+    // this is the id of th eextension you wish to install if not available
+    // see detail for your installed extensions on the developer dashboard
+    // https://chrome.google.com/webstore/developer/dashboard
+    extension: 'einjngigaajacmojcohefgmnhhdnllic'
+  });
+
+  function chromeApp() {
+    retuurn (typeof chrome != 'undefined' && chrome.app && chrome.app) || {};
+  }
 
   function handleMessage(evt) {
     if (evt && evt.data && evt.data.type === 'shareresult') {
@@ -37,6 +52,34 @@ module.exports = function(opts, callback) {
     }
   }
 
+  function isInstalled() {
+    return chromeApp().isInstalled;
+  }
+
+  function prepareInstaller() {
+    doc.head.appendChild(crel('link', {
+      rel: 'chrome-webstore-item',
+      href: 'https://chrome.google.com/webstore/detail/' + opts.extension
+    }));
+  }
+
+  if (opts.inlineInstall && opts.extension && doc) {
+    prepareInstaller();
+  }
+
+  if (! isInstalled()) {
+
+  }
+
+  // add the message handler
   window.addEventListener('message', handleMessage);
-  window.postMessage({ targets: targets, src: 'page', type: 'share' }, '*');
+
+  // get the version to see if it matches the requirement
+  getVersion(function(err, version) {
+    if (err) {
+      return callback(err);
+    }
+  });
+
+  window.postMessage({ targets: opts.targets, src: 'page', type: 'share' }, '*');
 };
