451123377020035785fb8898593143bd9e7fff30
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-14 09:40:51 +1100

    Updated share window example to allow extension insteallation and detection

--- examples/share-window.js
+++ examples/share-window.js
@@ -1,18 +1,50 @@
-var screenshare = require('..');
 var media = require('rtc-media');
+var h = require('hyperscript');
+var crel = require('crel');
+var screenshare = require('..')({
+  chromeExtension: 'rtc.io screenshare',
+  version: '^1.0.0'
+});
 
-screenshare(function(err, constraints) {
+var buttons = {
+  install: h('button', 'Install Extension', { onclick: function() {
+    chrome.webstore.install();
+  }}),
+
+  capture: h('button', 'Capture Screen', { onclick: shareScreen })
+};
+
+function shareScreen() {
+  screenshare.request(function(err, constraints) {
+    if (err) {
+      return console.error('Could not capture window: ', err);
+    }
+
+    console.log('attempting capture with constraints: ', constraints);
+    media({
+      constraints: constraints,
+      target: document.getElementById('main')
+    });
+  });
+}
+
+// detect whether the screenshare plugin is available and matches
+// the required version
+screenshare.available(function(err, version) {
+  var actions = document.getElementById('actions');
+
   if (err) {
-    return console.error('Could not capture window: ', err);
+    return actions.appendChild(buttons.install);
   }
 
-  console.log('attempting capture with constraints: ', constraints);
-  media({ constraints: constraints })
-    .on('error', function(err) {
-      console.error('Could not capture: ', err);
-    })
-    .once('capture', function(stream) {
-      console.log('Successfully captured stream: ', stream);
-    })
-    .render(document.body);
+  actions.appendChild(buttons.capture);
+});
+
+// on install show the capture button and remove the install button if active
+screenshare.on('install', function() {
+  if (buttons.install.parentNode) {
+    buttons.install.parentNode.removeChild(buttons.install);
+  }
+
+  document.getElementById('actions').appendChild(buttons.capture);
 });
