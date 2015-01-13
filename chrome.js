4728f64f1eb9de81751971a39680b15d01fe0a80
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-14 09:41:50 +1100

    Use chromex client for extension comms

--- chrome.js
+++ chrome.js
@@ -1,36 +1,22 @@
-var defaults = require('cog/defaults');
-var crel = require('crel');
+var extend = require('cog/extend');
+var OPT_DEFAULTS = {
+  target: 'rtc.io screenshare'
+};
 
-module.exports = function(opts, callback) {
-  var doc = typeof document != 'undefined' && document;
+module.exports = function(opts) {
+  var extension = require('chromex/client')(extend({}, OPT_DEFAULTS, opts, {
+    target: (opts || {}).chromeExtension
+  }));
 
-  // remap args
-  if (typeof opts == 'function') {
-    callback = opts;
-    opts = {};
-  }
+  extension.available = function(callback) {
+    return extension.satisfies((opts || {}).version, callback);
+  };
 
-  // initialise defaults
-  opts = defaults({}, opts, {
-    targets: ['window', 'screen'],
-    inlineInstall: true,
-
-    // this is the id of th eextension you wish to install if not available
-    // see detail for your installed extensions on the developer dashboard
-    // https://chrome.google.com/webstore/developer/dashboard
-    extension: 'einjngigaajacmojcohefgmnhhdnllic'
-  });
-
-  function chromeApp() {
-    retuurn (typeof chrome != 'undefined' && chrome.app && chrome.app) || {};
-  }
-
-  function handleMessage(evt) {
-    if (evt && evt.data && evt.data.type === 'shareresult') {
-      window.removeEventListener('message', handleMessage);
-
-      if (evt.data.error) {
-        return callback(new Error(evt.data.error.message || evt.data.error));
+  // patch in our capture function
+  extension.request = function(callback) {
+    extension.sendCommand('share', function(err, sourceId) {
+      if (err) {
+        return callback(err);
       }
 
       // pass the constraints through
@@ -39,7 +25,7 @@ module.exports = function(opts, callback) {
         video: {
           mandatory: {
             chromeMediaSource: 'desktop',
-            chromeMediaSourceId: evt.data.sourceId,
+            chromeMediaSourceId: sourceId,
             maxWidth: screen.width,
             maxHeight: screen.height,
             minFrameRate: 1,
@@ -49,37 +35,8 @@ module.exports = function(opts, callback) {
           optional: []
         }
       });
-    }
-  }
+    });
+  };
 
-  function isInstalled() {
-    return chromeApp().isInstalled;
-  }
-
-  function prepareInstaller() {
-    doc.head.appendChild(crel('link', {
-      rel: 'chrome-webstore-item',
-      href: 'https://chrome.google.com/webstore/detail/' + opts.extension
-    }));
-  }
-
-  if (opts.inlineInstall && opts.extension && doc) {
-    prepareInstaller();
-  }
-
-  if (! isInstalled()) {
-
-  }
-
-  // add the message handler
-  window.addEventListener('message', handleMessage);
-
-  // get the version to see if it matches the requirement
-  getVersion(function(err, version) {
-    if (err) {
-      return callback(err);
-    }
-  });
-
-  window.postMessage({ targets: opts.targets, src: 'page', type: 'share' }, '*');
+  return extension;
 };
