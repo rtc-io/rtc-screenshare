4b2e3099547c03e9d2cfce5ecdb6a78151234554
Damon Oehlman <damon.oehlman@gmail.com>
2015-01-14 09:41:36 +1100

    Changes to work with chromex for extension simplification

--- extension/src/index.js
+++ extension/src/index.js
@@ -1,35 +1,20 @@
-/* jshint node: true */
-'use strict';
-
 var extend = require('cog/extend');
+var extension = require('chromex')({
+  manifest: require('./manifest.json')
+});
 
-function handleRequest(request, sender, sendResponse) {
-  // if we have had a device requested, then capture that device and
-  // send the deviceid back in the response
-  if (request.type === 'share') {
-    capture(request.targets, sender, sendResponse);
-  }
-}
+// handle share events
+extension.on('share', function(opts, port, callback) {
+  var targets = (opts || {}).targets || ['window', 'screen'];
 
-function capture(targets, sender, callback) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  console.log('received share request');
+
+  chrome.desktopCapture.chooseDesktopMedia(targets, port.sender.tab, function(id) {
     if (chrome.runtime.lastError) {
       return callback({ error: chrome.runtime.lastError });
     }
 
-    // update the callback to directly send the message to the calling tab
-    callback = function(payload) {
-      chrome.tabs.sendMessage(tabs[0].id, extend(payload, { type: 'shareresult' }));
-    };
-
-    chrome.desktopCapture.chooseDesktopMedia(targets, tabs[0], function(id) {
-      if (chrome.runtime.lastError) {
-        return callback({ error: chrome.runtime.lastError });
-      }
-
-      callback({ sourceId: id });
-    });
+    console.log('success - stream id: ', id);
+    callback(null, id);
   });
-}
-
-chrome.runtime.onMessage.addListener(handleRequest);
+});
